8d68296ed8cd7fcdd55692be0f3966ebe04bdf64
supermepsipax <ttb.batchelor@gmail.com>
2023-05-09 15:05:52 -0300

    Trialling dead reckoning

--- src/eced3901_dt1.cpp
+++ src/eced3901_dt1.cpp
@@ -55,6 +55,7 @@ class SquareRoutine : public rclcpp::Node
 	{
 		x_now = msg->pose.pose.position.x;
 		y_now = msg->pose.pose.position.y;
+		theta_now = tf2::getYaw(msg->pose.pose.orientation);
 		
 		//RCLCPP_INFO(this->get_logger(), "Odom Acquired.");
 	}
@@ -67,7 +68,14 @@ class SquareRoutine : public rclcpp::Node
 		d_now =	pow( pow(x_now - x_init, 2) + pow(y_now - y_init, 2), 0.5 );
 		
 		// Keep moving if not reached last distance target
-		if (d_now < d_aim)
+		if (abs(theta_target - theta_now)< 0.05)
+		{
+			msg.linear.x = 0;
+			msg.angular.z = theta_vel;
+			publisher_->publish(msg);
+		}
+
+		else if (d_now < d_aim)
 		{	
 
 			msg.linear.x = x_vel;
@@ -98,23 +106,15 @@ class SquareRoutine : public rclcpp::Node
 			switch(count_) 
 			{
 			  case 0:
-			  	msg.angular.z = -1.57;
-			  	publisher_->publish(msg);
 			    move_distance(1.0);
 			    break;
 			  case 1:
-			  	msg.angular.z = -1.57;
-			  	publisher_->publish(msg);
 			    move_distance(1.0);
 			    break;
 			  case 2:
-			  	msg.angular.z = -1.57;
-			  	publisher_->publish(msg);
 			    move_distance(1.0);
 			    break;
 			  case 3:
-			  	msg.angular.z = -1.57;
-			  	publisher_->publish(msg);
 			    move_distance(1.0);
 			    break; 
 			  default:
@@ -123,12 +123,14 @@ class SquareRoutine : public rclcpp::Node
 		}			
 	}
 	
-	// Set the initial position as where robot is now and put new d_aim in place	
+	// Set the initial position as where robot is now and put new d_aim in place
+
 	void move_distance(double distance)
 	{
 		d_aim = distance;
 		x_init = x_now;
-		y_init = y_now;		
+		y_init = y_now;
+		theta_target = theta_now + 1.57
 		count_++;		// advance state counter
 		last_state_complete = 0;	
 	}
@@ -145,8 +147,9 @@ class SquareRoutine : public rclcpp::Node
 	
 	// Declaration of Class Variables
 	double x_vel = 0.2;
-	double y_vel = 0.2;
+	double theta_vel = 0.5;
 	double x_now = 0, x_init = 0, y_now = 0, y_init = 0;
+	double theta_now = 0, theta_target = 0;
 	double d_now = 0, d_aim = 0;
 	size_t count_ = 0;
 	int last_state_complete = 1;
