aee1eef0f03344d591ed9afbc8b7fbeb1ddac7eb
supermepsipax <ttb.batchelor@gmail.com>
2023-05-09 19:57:12 -0300

    Trialling dead reckoning

--- src/eced3901_dt1.cpp
+++ src/eced3901_dt1.cpp
@@ -49,7 +49,7 @@ class SquareRoutine : public rclcpp::Node
 		publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel",10);
       
 	  	// Create the timer
-	  	timer_ = this->create_wall_timer(50ms, std::bind(&SquareRoutine::timer_callback, this)); 	  
+	  	timer_ = this->create_wall_timer(100ms, std::bind(&SquareRoutine::timer_callback, this)); 	  
 	}
 
   private:
@@ -75,20 +75,18 @@ class SquareRoutine : public rclcpp::Node
 		d_now =	pow( pow(x_now - x_init, 2) + pow(y_now - y_init, 2), 0.5 );
 		
 		// Keep moving if not reached last distance target
-		msg.angular.z = 0;
-		publisher_->publish(msg);
 		if (theta_now < -0.5)
 		{
 			theta_now = 6.28318 + theta_now;
 		}
-		if (theta_now < theta_min)
+		if (theta_now < (theta_target*0.95))
 		{
 			msg.linear.x = 0;
 			msg.angular.z = theta_vel;
 			publisher_->publish(msg);
 		}
 		
-		else if (theta_now > theta_max)
+		else if (theta_now > (theta_target*1.05))
 		{
 			msg.linear.x = 0;
 			msg.angular.z = -theta_vel;
@@ -127,23 +125,19 @@ class SquareRoutine : public rclcpp::Node
 			switch(count_) 
 			{
 			  case 0:
-			  	theta_min = -0.025;
-				theta_max = 0.025;
+			  	theta_target = 0;
 			    move_distance(1.0);
 			    break;
 			  case 1:
-			  	theta_min = 1.545;
-				theta_max = 1.595;
+			  	theta_target = 1.57;
 			    move_distance(1.0);
 			    break;
 			  case 2:
-			  	theta_min = 3.115;
-				theta_max = 3.165;
+			  	theta_target = 3.14;
 			    move_distance(1.0);
 			    break;
 			  case 3:
-			  	theta_min = 4.510;
-				theta_max = 4.560;
+			  	theta_target = 4.71;
 			    move_distance(1.0);
 			    break; 
 			  default:
@@ -175,9 +169,9 @@ class SquareRoutine : public rclcpp::Node
 	
 	// Declaration of Class Variables
 	double x_vel = 0.2;
-	double theta_vel = 0.5;
+	double theta_vel = 0.4;
 	double x_now = 0, x_init = 0, y_now = 0, y_init = 0;
-	double theta_now = 0, theta_min = 0, theta_max = 0.1;
+	double theta_now = 0, theta_target = 0;
 	double d_now = 0, d_aim = 0;
 	size_t count_ = 0;
 	int last_state_complete = 1;
