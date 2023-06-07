c05f7a30512a7accabf4448227ca1d3e7553dfe5
Tristan Batchelor <tr26334@dal.ca>
2023-06-07 16:56:18 -0300

    Updating nav_2 params to better avoid obstacles

--- scripts/demo_inspection.py
+++ scripts/demo_inspection.py
@@ -76,7 +76,8 @@ def main():
     # Inspection route, probably read in from a file for a real application
     # from either a map or drive and repeat.
     inspection_route = [
-        [1.0, 0.0],
+        [0.0, 0.2],
+        [1.0, 0.2],
         [1.0, 1.0],
         [0.0, 1.0],
         [0.0, 0.0]
@@ -100,8 +101,8 @@ def main():
     inspection_pose = PoseStamped()
     inspection_pose.header.frame_id = 'map'
     inspection_pose.header.stamp = navigator.get_clock().now().to_msg()
-    inspection_pose.pose.orientation.z = 0.0
-    inspection_pose.pose.orientation.w = 1.0
+    #inspection_pose.pose.orientation.z = 0.0
+    #inspection_pose.pose.orientation.w = 1.0
     for pt in inspection_route:
         inspection_pose.pose.position.x = pt[0]
         inspection_pose.pose.position.y = pt[1]
