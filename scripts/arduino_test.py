24afd137d5eb21da1b0cee673b2df7772fa4bd5e
Tristan Batchelor <tr26334@dal.ca>
2023-06-29 21:53:06 -0300

    Adding arduino test script for serial communication

---
+++ scripts/arduino_test.py
@@ -0,0 +1,17 @@
+import serial
+import time
+
+nano = serial.Serial('/dev/ttyUSB0', 9600, timeout = 1)
+time.sleep(1)
+nano.readline()
+for i in range(8):
+    nano.write(b'humidity')
+    time.sleep(0.12)
+    byte_string = nano.readline()
+    reply = str(byte_string).strip("b'\\rn")
+
+    print(reply)
+
+nano.close()
+
+
