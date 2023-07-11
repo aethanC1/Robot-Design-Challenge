a3026d215869999135dcef2d157adb2d9df4503b
supermepsipax <ttb.batchelor@gmail.com>
2023-07-11 13:14:28 -0300

    Adding light switching functionality and new challenge course launch file

--- Arduino Scripts/arduino_design_II/arduino_design_II.ino
+++ Arduino Scripts/arduino_design_II/arduino_design_II.ino
@@ -1,4 +1,4 @@
-
+#include <Adafruit_NeoPixel.h>
 float base = 0;
 float base1 = 0;
 float base2 = 0;
@@ -6,21 +6,19 @@ float base3 = 0;
 float current = 0;
 int ground_sample = 0;
 int humidity_sample = false;
-int input = 15;
+int input = 16;
 int wood = 10;
 int pipe = 11;
 int vent = 6;
 float delta = 0;
+int ring = 3;
+int numLed = 12;
+Adafruit_NeoPixel strip(numLed, ring, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
+  strip.begin();
   Serial.begin(9600);
-  base1 = analogRead(input);//arduino takes initial voltage reading, this will be the baseline and the measurement for the wood surface
-  delay(20);
-  base2 = analogRead(input);
-  delay(20);
-  base3 = analogRead(input);
-  base = (base1 + base2 + base3)/3;
   pinMode(pipe, OUTPUT);
   pinMode(wood, OUTPUT);
   pinMode(vent, OUTPUT);
@@ -30,7 +28,7 @@ void setup()
 void material(float change)
 //function takes change in voltage and determines the surface it is above
 {
-  if(change>=100)
+  if(change>=60)
   //if delta increases photo resisor is exposed to less light
   {
     Serial.println("grate");//light decrease means surface is darker, meaning the vent
@@ -38,7 +36,7 @@ void material(float change)
     digitalWrite(wood,0);
     digitalWrite(pipe,0);
   }
-  else if(change<=-50)
+  else if(change<=-40)
   {
     Serial.println("pipe");// increase in light, means more reflection, meaning the pipe
     digitalWrite(vent, 0);
@@ -53,7 +51,21 @@ void material(float change)
     digitalWrite(pipe,0);
   }
 }
-
+void light_on()
+{
+  for (int i = 0; i < numLed; i++) {
+    strip.setPixelColor(i, strip.Color(50, 50, 50));
+  }
+  strip.show(); // Display the updated colors on the LEDs
+}
+void light_off()
+{
+  for (int i = 0; i < numLed; i++) 
+  {
+    strip.setPixelColor(i, strip.Color(0, 0, 0));
+  }
+   strip.show(); // Display the updated colors on the LEDs
+}
 void loop()
 {
   while (Serial.available() == 0){}
@@ -73,6 +85,22 @@ void loop()
   }
   if(command == "all_clear"){
     Serial.println("green_light_on");
+  }
+  if(command == "baseline"){
+    base1 = analogRead(input);//arduino takes initial voltage reading, this will be the baseline and the measurement for the wood surface
+    delay(20);
+    base2 = analogRead(input);
+    delay(20);
+    base3 = analogRead(input);
+    base = (base1 + base2 + base3)/3;
+  }
+  if(command == "light_on"){
+    light_on();
+    Serial.println("light_on");
+  }
+  if(command == "light_off"){
+    light_off();
+    Serial.println("light_off");
   }
 
   delay(100);
---
+++ scripts/just_do_it.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python3
+from copy import deepcopy
+from geometry_msgs.msg import PoseStamped, Quaternion, Twist
+from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
+import math
+import serial
+import rclpy
+import time
+from rclpy.node import Node
+
+class challenge_node(Node):
+
+    def __init__(self):
+        super().__init__('challenge_node')
+        self.navigator = BasicNavigator()
+        self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.message = Twist()
+        try:
+        	self.nano = serial.Serial('/dev/ttyUSB0', 9600, timeout = 1)
+        except Exception:
+        	self.nano = serial.Serial('/dev/ttyUSB4', 9600, timeout = 1)
+        	
+        time.sleep(1)
+        self.nano.readline()
+    	#Points for route, points are x, y, yaw respectively
+        point_1 = [[0.0, 0.0, 1.57]]
+        point_2 = [[1.72, 1.72, 0.75]]        
+        #point_3 = [[1.3, 3.33, 1.57],[3.0, 3.35, 0]]
+        #point_4 = [[1.3, 3.33, 3.14],[1.3, 1.2, -1.57]]
+        #point_5 = [[1.3, 0.7, -1.57]]
+        #point_6 = [[1.8, 1.83, 1.57]]        
+        #point_7 = [[3.6, 0.0, 0.0]]
+        self.inspection_route = [point_1, point_2]#, point_3, point_4, point_5, point_6, point_7]
+    	# Wait for navigation to fully activate
+        self.navigator.waitUntilNav2Active()
+        self.main()
+
+    def euler_from_quaternion(self, x, y, z, w):
+            """
+            Convert a quaternion into euler angles (roll, pitch, yaw)
+            roll is rotation around x in radians (counterclockwise)
+            pitch is rotation around y in radians (counterclockwise)
+            yaw is rotation around z in radians (counterclockwise)
+            """
+            t0 = 2.0 * (w * x + y * z)
+            t1 = 1.0 - 2.0 * (x * x + y * y)
+            roll_x = math.atan2(t0, t1)
+        
+            t2 = +2.0 * (w * y - z * x)
+            t2 = +1.0 if t2 > +1.0 else t2
+            t2 = -1.0 if t2 < -1.0 else t2
+            pitch_y = math.asin(t2)
+        
+            t3 = +2.0 * (w * z + x * y)
+            t4 = +1.0 - 2.0 * (y * y + z * z)
+            yaw_z = math.atan2(t3, t4)
+        
+            return roll_x, pitch_y, yaw_z # in radians
+            
+    def quaternion_from_euler(self, roll, pitch, yaw):
+        """
+        Convert euler angles back into a quaternion to be
+        used for orientation in ros2
+        """
+        cos_y = math.cos(yaw * 0.5)
+        sin_y = math.sin(yaw * 0.5)
+        cos_p = math.cos(pitch * 0.5)
+        sin_p = math.sin(pitch * 0.5)
+        cos_r = math.cos(roll * 0.5)
+        sin_r = math.sin(roll * 0.5)
+
+        w = cos_r * cos_p * cos_y + sin_r * sin_p * sin_y
+        x = sin_r * cos_p * cos_y - cos_r * sin_p * sin_y
+        y = cos_r * sin_p * cos_y + sin_r * cos_p * sin_y
+        z = cos_r * cos_p * sin_y - sin_r * sin_p * cos_y
+
+        return [x, y, z, w]
+
+    def navigate_to(self, route):
+        """
+        Takes a list of lists in the form of x,y,theta coordinates 
+        navigates to each point in the list, uses ros2 nav2 for 
+        waypoint navigation
+
+        -------------------
+        Returns 
+        True if navigation successful
+        False if navigation unsuccessful
+        """
+
+        waypoints = []
+        pose = PoseStamped()
+        pose.header.frame_id = 'map'
+        pose.header.stamp = self.navigator.get_clock().now().to_msg()
+        for point in route:
+            x, y, z, w = self.quaternion_from_euler(0, 0, point[2])
+            pose.pose.position.x = point[0]
+            pose.pose.position.y = point[1]
+            pose.pose.orientation.x = x
+            pose.pose.orientation.y = y
+            pose.pose.orientation.z = z
+            pose.pose.orientation.w = w
+            waypoints.append(deepcopy(pose))
+        self.navigator.followWaypoints(waypoints)
+
+        i = 0
+        while not self.navigator.isTaskComplete():
+            i += 1
+            feedback = self.navigator.getFeedback()
+            if feedback and i % 5 == 0:
+                print('Executing current waypoint: ' +
+                    str(feedback.current_waypoint + 1) + '/' + str(len(waypoints)))
+
+        result = self.navigator.getResult()
+        if result == TaskResult.SUCCEEDED:
+            return True
+        elif result == TaskResult.CANCELED:
+            return False
+        elif result == TaskResult.FAILED:
+            return False
+        
+    def recover_position(self, position):
+        """
+        In the event the robot can't get to a certain point,
+        will set a waypoint to the last valid coordinate it was
+        at so it can try again
+        """
+
+        last_valid_position = position[len(position) - 1]
+        pose = PoseStamped()
+        x, y, z, w = self.quaternion_from_euler(0, 0, last_valid_position[2])
+        pose.pose.position.x = last_valid_position[0]
+        pose.pose.position.y = last_valid_position[1]
+        pose.pose.orientation.x = x
+        pose.pose.orientation.y = y
+        pose.pose.orientation.z = z
+        pose.pose.orientation.w = w
+        pose.append(deepcopy(pose))
+        self.navigator.goToPose(pose)
+        while not self.navigator.isTaskComplete():
+            pass
+
+    def response_listen(self, timeout_limit = 25):
+        """
+        Will wait for a response and return a formatted string from
+        the serial output buffer of the connected arduino.
+        
+        Parameters
+        ------------------------------------------------
+        timeout_limit: the max number of 10ms sleep cycles it waits
+        for a response, default is 25 cycles/250 ms
+
+        ------------------------------------------------
+        Returns:
+        response: string recieved from arduino serial comm
+        """
+        timeout = 0
+        response = ''
+        while not self.nano.in_waiting:
+            if timeout == timeout_limit:
+                return response
+            timeout += 1
+            time.sleep(0.01)
+        else:
+            response = self.nano.readline().decode('utf-8').rstrip()
+            return response
+
+
+    def detect_ground(self):
+        """
+        Continously queries the pipe/ground detector subsystem to
+        see if the sensor is positioned over the pipe/grate, will either
+        stop when over the pipe, or after approximate timeout of 5 seconds
+        """
+        grate = False
+        timeout = 0
+        self.nano.write(b'light_on')
+        self.response_listen(100)
+        self.nano.write(b'ground')
+        ground = self.response_listen()
+        while ground == 'wood' and timeout < 25 and not grate:
+            self.nano.write(b'ground')
+            ground = self.response_listen()
+            if ground == 'grate':
+                grate = True
+            timeout += 1
+        self.nano.write(b'light_off')
+        self.response_listen(100)
+
+    def detect_humidity(self):
+        """
+        Queries the humidity sensor multiple times to get multiple readings,
+        if the readings are over a certain threshold will return an indication 
+        leak is detected
+
+        -----------------------------------------------------------------
+        Returns:
+            True if leak detected
+            False otherwise
+        """
+        num_samples = 0
+        positive_samples = 0
+        while num_samples < 10:
+            self.nano.write(b'humidity')
+            result = self.response_listen()
+            if result == "true":
+                positive_samples += 1
+            num_samples += 1
+            if positive_samples > 3:
+                return True
+        return False
+    
+    def main(self):
+        """
+        Goes through a list of points on the pipeline and feeds them into a waypoint
+        follower, if the follower is successful, begins leak detection/mitagion procedures
+        if unable to navigate, will try to navigate back to last valid position and then continue
+        on
+        """
+        initial_pose = PoseStamped()
+        initial_pose.header.frame_id = 'map'
+        initial_pose.header.stamp = self.navigator.get_clock().now().to_msg()
+        initial_pose.pose.position.x = 0.0
+        initial_pose.pose.position.y = 0.0
+        initial_pose.pose.orientation.z = 0.0
+        initial_pose.pose.orientation.w = 1.0
+        self.navigator.setInitialPose(initial_pose)
+        max_steps = len(self.inspection_route)
+        step = 0
+        while step < max_steps:
+            if self.navigate_to(self.inspection_route[step]):
+                if not step:
+                    self.nano.write(b'light_on')
+                    self.response_listen(100)
+                    self.nano.write(b'baseline')
+                    self.response_listen(100)
+                    self.nano.write(b'light_off')
+                    self.response_listen(100)
+                else:
+                    self.message.angular.z = 0.1
+                    self.publisher.publish(self.message)
+                    self.detect_ground()
+                    self.message.angular.z = 0.0
+                    self.publisher.publish(self.message)
+                    leak = self.detect_humidity()
+                    if leak:
+                        self.nano.write(b'mitigate')
+                        self.response_listen(timeout = 200)
+                    else:
+                        self.nano.write(b'all_clear')
+                    step += 1
+            else:
+                if step:
+                    self.recover_position(self.inspection_route[step - 1])
+                else:
+                    self.navigator.goToPose(initial_pose)
+                    while not self.navigator.isTaskComplete():
+                        pass
+                
+
+        
+
+
+if __name__ == '__main__':
+    rclpy.init()
+    _challenge = challenge_node()
+    rclpy.spin(_challenge)
+    rclpy.shutdown()
+    
